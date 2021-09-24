92f6a4caafae5ade22a661f7b9dfba66283cce86
Gapur Yovloy <kaloycant@gmail.com>
2021-09-24 23:43:12 +0300

    start gsap module

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,3 +1,20 @@
 import barba from '@barba/core';
 import gsap from 'gsap';
 
+
+// init Barba
+barba.init({
+    transitions: [{
+      name: 'opacity-transition',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
+    }]
+  });
